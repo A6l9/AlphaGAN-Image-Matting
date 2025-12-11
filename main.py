33e5be01952d76467fb3de253f14873b55cfc9a7
A6l9 <sfsf.sfsf.18@list.ru>
2025-12-11 15:32:48 +0100

    Implement function to prepare training pipeline and define training loop

--- main.py
+++ main.py
@@ -1,30 +1,139 @@
 from pathlib import Path
 
 import torch as tch
+from tqdm import tqdm
+import torch.nn as nn
+from torch import optim
+from torch.optim.lr_scheduler import CyclicLR
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 
-from models import AlphaGenerator
+import models as mdl
 import utils as utl
 from dataset import CustomDataset
+from cfg_loader import cfg
+from transforms import TransformsPipeline
+import losses as ls
+from train import train_pipeline
+from schemas import TrainComponents
 
 
-def main() -> None:
-    csv_path = Path(__file__).parent / "dataset" / "dataset_labels.csv"
+def main(csv_path: Path) -> None:
+    """Prepares components and setup the train loop
 
-    dataset = CustomDataset(csv_path, train=True)
+    Args:
+        csv_path (Path): The path to the dataset labels
+    """
+    # Define an available device
+    DEVICE = tch.device("cuda" if tch.cuda.is_available() else "cpu")
+    
+    # Define model and dicsriminator
+    model = mdl.AlphaGenerator()
+    model.to(DEVICE)
 
-    sample, mask = dataset[75]
+    discriminator = mdl.PatchGANDiscriminator(4, nn.BatchNorm2d)
+    discriminator.to(DEVICE)
 
-    print(sample.shape)
-    print(mask.shape)
-    # model = AlphaGenerator()
+    # Prepare train and test datasets
+    tfs = TransformsPipeline()
+    dataset_train = CustomDataset(csv_path, cfg, train=True, transforms=tfs)
+    train_dataloader = DataLoader(
+        dataset_train,
+        batch_size=cfg.general.batch_size,
+        shuffle=True,
+        num_workers=utl.get_num_workers(),
+        pin_memory=True
+    )
+    dataset_test = CustomDataset(csv_path, cfg, train=False, transforms=tfs)
+    test_dataloader = DataLoader(
+        dataset_test,
+        batch_size=cfg.general.batch_size,
+        shuffle=False,
+        num_workers=utl.get_num_workers(),
+        pin_memory=True
+    )
 
-    # utl.set_seed()
+    # Define optimizer and scheduler for the discriminator and the model
+    g_optimizer = optim.AdamW(model.parameters(),
+                            lr=cfg.train.scheduler.start_lr,
+                            weight_decay=cfg.train.optimizer.weight_decay
+                            )
+    g_scheduler = CyclicLR(
+                        g_optimizer,
+                        base_lr=cfg.train.scheduler.start_lr,
+                        max_lr=cfg.train.scheduler.end_lr, 
+                        step_size_up=cfg.train.scheduler.step_size_up,
+                        mode='triangular'
+                    )
+    
+    d_optimizer = optim.AdamW(discriminator.parameters(),
+                            lr=cfg.train.scheduler.start_lr,
+                            weight_decay=cfg.train.optimizer.weight_decay
+                            )
+    d_scheduler = CyclicLR(
+                        d_optimizer,
+                        base_lr=cfg.train.scheduler.start_lr,
+                        max_lr=cfg.train.scheduler.end_lr, 
+                        step_size_up=cfg.train.scheduler.step_size_up,
+                        mode='triangular'
+                    )
+    
+    # Define losses
+    l_alpha_loss = ls.LAlphaLoss()
+    l_comp_loss = ls.LCompositeLoss()
+    gan_loss = ls.GANLoss()
+        
+    # Define the checkpoints dir and the logging dir paths
+    checkpoints_dir = Path(cfg.general.checkpoints_dir).absolute()
 
-    # x = tch.randn(1, 3, 1024, 1024)
+    # Load the last checkpoint
+    checkpoint = utl.load_checkpoint(checkpoints_dir, DEVICE)
 
-    # with tch.no_grad():
-    #     out = model(x)
+    if checkpoint:
+        utl.color("Checkpoint found, loading states...", "green")
+
+        model.load_state_dict(checkpoint["model_state"])
+        discriminator.load_state_dict(checkpoint["discriminator_state"])
+
+        g_optimizer.load_state_dict(checkpoint["g_optimizer_state"])
+        d_optimizer.load_state_dict(checkpoint["d_optimizer_state"])
+
+        g_scheduler.load_state_dict(checkpoint["g_scheduler_state"])
+        d_scheduler.load_state_dict(checkpoint["d_scheduler_state"])
+
+        curr_epoch = checkpoint["epoch"] + 1
+    else:
+        utl.color("No checkpoint found, starting from scratch.", "green")
+        curr_epoch = 0
+
+    # Define progress bar
+    prog_bar = tqdm(initial=curr_epoch, total=cfg.train.epoches, unit="epoch", desc="Training...")
+
+    # Create the tb logger
+    with SummaryWriter(Path(cfg.train.logging.log_dir)) as writer:
+        # Package it for train pipeline
+        components: TrainComponents = {
+            "device": DEVICE,
+            "model": model,
+            "epoch": curr_epoch,
+            "prog_bar": prog_bar,
+            "discriminator": discriminator,
+            "train_loader": train_dataloader,
+            "test_loader": test_dataloader,
+            "g_optimizer": g_optimizer,
+            "g_scheduler": g_scheduler,
+            "d_optimizer": d_optimizer,
+            "d_scheduler": d_scheduler,
+            "l_alpha_loss": l_alpha_loss,
+            "l_comp_loss": l_comp_loss,
+            "gan_loss": gan_loss,
+            "writer": writer
+        }
+
+        # Start the train pipeline
+        train_pipeline(components)
 
 
 if __name__ == "__main__":
-    main()
+    csv_path = Path(__file__).parent / "dataset" / "dataset_labels.csv"
+    main(csv_path)
